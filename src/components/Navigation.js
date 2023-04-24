b890cf8bbfc9632e5d5c3ba3b41bd0a9867ec053
JSB95 <vetements@kakao.com>
2023-04-24 16:45:09 +0900

    nav bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css"
+
+function Navigation() {
+    return (
+        <div className={nav}>
+
+
+        </div>
+    )
+}
